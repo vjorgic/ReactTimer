a668719fd0205d503f02133e13fe666f068b2aa6
vjorgic <vjorgic@gmail.com>
2017-06-29 13:59:26 -0600

    Initial Commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var Navigation = require('Navigation');
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div>
+          <div>
+            <Navigation></Navigation>
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Main;
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = React.createClass({
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li><IndexLink to="/" activeClassName="active-link">Timer</IndexLink></li>
+            <li><Link to="/countdown" activeClassName="active-link">Countdown</Link> </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu=text">
+              Created by <a href="http://www.elitelogic.ca" target="_blank">Vlado</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Navigation;
