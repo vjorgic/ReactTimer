fcb0f1425e9a386e0ae7add176f79c4e16420a93
vjorgic <vjorgic@gmail.com>
2017-07-04 10:50:08 -0600

    Added controls compoent

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -2,7 +2,13 @@ var React = require('react');
 
 var Controls = React.createClass({
   propTypes: {
-    countdownStatus: React.PropTypes.string.isRequired
+    countdownStatus: React.PropTypes.string.isRequired,
+    onStatusChange: React.PropTypes.func.isRequired
+  },
+  onStatusChanged: function(newStatus) {
+    return () => {
+      this.props.onStatusChanged(newStatus);
+    }
   },
   render: function () {
     var {countdownStatus} = this.props;
@@ -10,16 +16,16 @@ var Controls = React.createClass({
     var renderStartStopButton = () => {
         debugger;
         if(countdownStatus === 'started') {
-          return <button className="button secondary">Pause</button>
+          return <button className="button secondary" onClick={this.onStatusChanged('paused')}>Pause</button>
         } else if(countdownStatus === 'paused') {
-          return <button className="button primary">Start</button>
+          return <button className="button primary" onClick={this.onStatusChanged('started')}>Start</button>
         }
     };
 
     return (
       <div className="controls">
         {renderStartStopButton()}
-        <button className="button alert hollow">Clear</button>
+        <button className="button alert hollow" onClick={this.onStatusChanged('stopped')}>Clear</button>
       </div>
     );
   }
